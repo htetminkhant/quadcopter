387b1f043daafc3393492a3672c0455750dcf758
htetminkhant <htetminkhant07@gmail.com>
2017-12-12 13:37:29 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,27 +1,30 @@
-// 2.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include "stdio.h"
 #include <stdlib.h>
 #include <conio.h>
 #include <iostream>
 #include <vector>
-
+using namespace std;
 int main()
 {
 	double start_time=0.0,end_time=10.0, dt=0.005;
+	int N=0;
 	std::vector<double>myvec;
-	do
-	{   start_time+=dt;
+	myvec.push_back(start_time);
+	while(start_time<=end_time)
+	{  
+		start_time+=dt;
 		myvec.push_back(start_time);
+		
+	}
+	for(int i=0;i<myvec.size();i++)
+	{   
+		++N;
+		std::cout << myvec[i]<<" ";
 
 	}
-	while(start_time<=end_time);
-	std::vector<double> :: iterator i;
-	for(int i=0;i<myvec.size();i++)
-	{
-		std::cout << myvec[i]<<" ";
-	}
+	cout<<endl;
+	cout<<"N"<<"="<<N;
+	cout<<endl;
 	return 0;
 }
