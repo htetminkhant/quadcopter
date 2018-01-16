ef162f3c02797fa7ee91f8f442f9a3f6a028d39b
htetminkhant <htetminkhant07@gmail.com>
2018-01-17 00:42:03 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -7,8 +7,9 @@
 #include <time.h>
 #include <math.h>
 using namespace std;
+void acceleration(double inputs,vector<double>theta,vector<double>xdot_,double m,double g,double k,double kd);
 void createvector(vector<double>&myvec);
-void thetadot2omega(vector<double>thetadot,vector<double>theta);
+void thetadot2omega(vector<double>thetadot,vector<double>angle);
 int main()
 {	
 	srand( time(0));
@@ -34,12 +35,33 @@ int main()
 		thetadot.push_back((temp*pi)/180);
 		cout<<thetadot[i];
 	};
+	double i,g=9.81,m=0.5,L=0.25,k=3e-6,b=1e-7,kd=0.25;
+	vector<vector<double>>I(3,vector<double>(3));
+	vector<vector<double>>omega(3,vector<double>(1));
+	I[0][0]=5e-3;
+	I[0][1]=0;
+	I[0][2]=0;
+	I[1][0]=0;
+	I[1][1]=5e-3;
+	I[1][2]=0;
+	I[2][0]=0;
+	I[2][1]=0;
+	I[2][2]=10e-3;
+	vector<double>omega_=thetadot2omega(thetadot,theta);
+	for (int j=0;j<N;j++)
+	{
+		i=timevector[j];
+		
+		acceleration(i,theta,xdot,m,g,k,kd);
+		
+	}
 	
-	thetadot2omega(thetadot,theta);
 	cout<<endl;
 	_getch();
 	return 0;
 }
+
+
 void createvector(vector<double>& myvec)
 {
 	double start_time=0.0,end_time=10.0, dt=0.005;
@@ -52,9 +74,10 @@ void createvector(vector<double>& myvec)
 	}
 
 }
-void thetadot2omega(vector<double>thetadot,vector<double>theta)
+
+void thetadot2omega(vector<double>thetadot,vector<double>angle)
 {
-	double phi = theta[0],theta_ = theta[1], psi = theta[2];
+	double phi = angle[0],theta_ = angle[1], psi = angle[2];
 	vector<vector<double>>w(3,vector<double>(3));
 	
 	w[0][0]=1;
@@ -79,4 +102,59 @@ void thetadot2omega(vector<double>thetadot,vector<double>theta)
 			cout << "omega "<<omega[i][j]<<"\n ";
 		}
 	}
+	return omega;
+}
+
+void acceleration(double inputs,vector<double>angle,vector<double>xdot_,double m,double g,double k,double kd)
+{
+	double phi = angle[2],theta_ = angle[1], psi = angle[0];
+	vector<vector<double>>R(3,vector<double>(3));
+	R[0][0]=cos(phi)*cos(theta_);
+	R[1][0]=cos(theta_)*sin(phi);
+	R[2][0]=-sin(theta_);
+	R[0][1]=cos(phi) * sin(theta_) * sin(psi) - cos(psi) * sin(phi);
+	R[1][1]=cos(phi) * cos(psi) + sin(phi) * sin(theta_) * sin(psi);
+	R[2][1]=cos(theta_)*sin(phi);
+	R[0][2]=sin(phi) * sin(psi) + cos(phi) * cos(psi) * sin(theta_);
+	R[1][2]=cos(psi) * sin(phi) * sin(theta_) - cos(phi) * sin(psi);
+	R[2][2]=cos(theta_)*cos(phi);
+	vector<double>timevector;
+	createvector(timevector);
+	double sumtime=0;
+	for(int i=0;i<timevector.size();i++)
+	{   
+		sumtime+=timevector[i];
+	}
+	vector<vector<double>>T(3,vector<double>(1));
+	T[0][0]=0;
+	T[1][0]=0;
+	T[2][0]=k*sumtime;
+	vector<vector<double>>gravity(3,vector<double>(1));
+	gravity[0][0]=0;
+	gravity[1][0]=0;
+	gravity[2][0]=-g;
+	vector<vector<double>>T_(3,vector<double>(1));
+	for (int i=0;i<3;i++)
+	{
+		for(int j=0;j<1;j++)
+		{
+			T_[i][j]=0;
+			for(int k=0;k<3;k++)
+			{
+				T_[i][j]=T_[i][j]+R[i][k]*T[k][j];
+			}
+		}
+	}
+	vector<vector<double>>FD(3,vector<double>(1));
+	vector<vector<double>>T2(3,vector<double>(1));
+	vector<vector<double>>a(3,vector<double>(1));
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<1;j++)
+		{
+			FD[i][j]=-kd*xdot_[i];
+			T2[i][j]=(1/m)*T_[i][j];
+			a[i][j]=gravity[i][j]+T2[i][j]+FD[i][j];
+		}
+	};
 }
